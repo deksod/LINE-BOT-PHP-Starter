2a06aeffd7bc023354956451c542d0c7975a3a51
deksod <30456589+deksod@users.noreply.github.com>
2017-07-26 22:23:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,50 +11,45 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
-$rand = rand(0,10);
+$rand = rand(1,12);
  
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+if($arrJson['events'][0]['message']['text'] == "สวัสดีบอท"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userDisplay'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-  $arrPostData['messages'][1]['type'] = "text";
-  $arrPostData['messages'][1]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else if($arrJson['events'][0]['message']['text'] == "สุ่ม" && $rand < 5){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "คุณ".$arrJson['events'][0]['source']['userId']."ทอยได้ ต่ำ";
-  $arrPostData['messages'][1]['type'] = "text";
-  $arrPostData['messages'][1]['text'] = $rand;
- }else if($arrJson['events'][0]['message']['text'] == "สุ่ม" && $rand >= 5){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สูง";
-  $arrPostData['messages'][1]['type'] = "text";
-  $arrPostData['messages'][1]['text'] = $rand;
-  $arrPostData['messages'][2]['type'] = "image";
-  $arrPostData['messages'][2]['originalContentUrl'] = "https://www.dropbox.com/s/7wp3bc4xwzyb2ni/image01.jpg";
-  $arrPostData['messages'][2]['previewImageUrl'] = "https://www.dropbox.com/s/7wp3bc4xwzyb2ni/image01.jpg";
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['displayName'];
 }
- 
- 
+}else if($arrJson['events'][0]['message']['text'] == "ใส่ชุดอะไรดี"){
+ if($rand > 1){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันว่า".$arrJson['events'][0]['source']['displayName']."เหมาะกับชุดนี้มากเลยนะ...";
+  $arrPostData['messages'][1]['type'] = "image";
+  $arrPostData['messages'][1]['originalContentUrl'] = "https://s-media-cache-ak0.pinimg.com/736x/9b/9c/9d/9b9c9d645da3c8188c22f13f2f97111c--boho-holiday-style-bohemian-bohemian-style-dress.jpg";
+  $arrPostData['messages'][1]['previewImageUrl'] = "https://s-media-cache-ak0.pinimg.com/736x/9b/9c/9d/9b9c9d645da3c8188c22f13f2f97111c--boho-holiday-style-bohemian-bohemian-style-dress.jpg";
+ }
+ else if($rand == 15){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันว่าคุณเหมาะกับชุดนี้มากเลยนะ...";
+  $arrPostData['messages'][1]['type'] = "image";
+  $arrPostData['messages'][1]['originalContentUrl'] = "";
+  $arrPostData['messages'][1]['previewImageUrl'] = "";
+ }
+ else if($rand == 16){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันว่าคุณเหมาะกับชุดนี้มากเลยนะ...";
+  $arrPostData['messages'][1]['type'] = "image";
+  $arrPostData['messages'][1]['originalContentUrl'] = "";
+  $arrPostData['messages'][1]['previewImageUrl'] = "";
+ }
+}
+
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
