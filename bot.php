ebc15a6284cce980c606b8519c940aa6e1a1af44
deksod <30456589+deksod@users.noreply.github.com>
2017-07-26 23:34:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
  
-$strAccessToken = "nkNwh0ENuXMxsOv6bC0u/lK/gEm8IHfk2s832s3d+IuApqDUBDoS2UKaquyfXjwfBUhOvsBWE3Iupe8wHOu4gWGETNzI/tYP8vEFdg58fGpYN0XP0/ow645TPfcChnhDM5OvGQNN0P18rTs7vYE/LwdB04t89/1O/w1cDnyilFU=";
+$strAccessToken = "EDRjdEljIpcgBC6/GRP3y90WSbdJcAKmMp0QAHcsAdNvs5u1yCI2dri9FEdL8Dph5kSoF9jkOa8lSRx3vlSWn4QFbYibfEL+GHnf7Ci7uCdK7HL9IpBS0BxuVFp+5LDkNBpyiEB3rGMkN24OOIxSrQdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
